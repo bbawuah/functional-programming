ac16b4721fc6aef2289ef19e3ea076ab9795affb
bbawuah <brianbawuah@icloud.com>
2020-10-23 10:50:23 +0200

    convert the strings that can be converted to numbers

--- dist/server.js
+++ dist/server.js
@@ -3,12 +3,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-var express_1 = __importDefault(require("express"));
-var constants_1 = require("./config/constants");
-var app = express_1.default();
-app.use(express_1.default.json());
-app.listen(constants_1.PORT, function () {
-    console.log("Server is listening on port " + constants_1.PORT);
+var Survey_Information_Design_clean_parsed_json_1 = __importDefault(require("./Survey_Information_Design_clean-parsed.json"));
+var arrayMetArrays = Survey_Information_Design_clean_parsed_json_1.default.map(function (data) {
+    return Object.entries(data);
 });
+var arrayMetNummers = arrayMetArrays.map(function (arrayInArray) {
+    var gefilterdeArray = arrayInArray.filter(function (data) {
+        var num = parseInt(data[1]);
+        if (!isNaN(num)) {
+            return data;
+        }
+    });
+    var convertedArray = gefilterdeArray.map(function (data) { return [
+        data[0],
+        parseInt(data[1])
+    ]; });
+    return convertedArray;
+});
+console.log(arrayMetNummers);
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,121 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import express from 'express'
-import { PORT } from './config/constants'
+import data from './Survey_Information_Design_clean-parsed.json'
 
-const app = express()
-app.use(express.json())
+interface DataType {
+  geboortedatum: string
+  geboorteplaats: string
+  hoeveelheidBroers: string
+  hoeveelheidZussen: string
+  hoeveelheidOomsEnTantes: string
+  rijbewijs: string
+  grootsteAutoVanOudersOfVerzorgers: string
+  bedragKwijtAanAbonnementen: string
+  oogKleur: string
+  beroepMoeder: string
+  beroepVader: string
+  huisDieren: string
+  hobbies: string
+  naamAnderGeslacht: string
+  grootteSlaapkamer: string
+  grootteBureau: string
+  hoeveelheidKledingstukken: string
+  bedragPerMaandAanKleding: string
+  hoeveelPaarSchoenen: string
+  schoenmaat: string
+  cijferGezondheid: string
+  cijferStressniveau: string
+  politiekeVoorkeur: string
+  relatie: string
+  mensenAangesprokenMetU: string
+  vooropleiding: string
+  studiepunten: string
+  leuksteCMDVak: string
+  naCMDDoen: string
+  voorkeuren: string
+  CMDVoorkeuren: string
+  bijbaan: string
+  urenPerWeekBesteedAanBijbaan: string
+  waaromGekozenVoorEigenTrack: string
+  watWilJeWorden: string
+  hoeveelPerMaandWillenVerdienen: string
+  hoogteStudieschuld: string
+  reistijdNaarSchool: string
+  belangrijksteEigenschapOntwerper: string
+  lievelingsboek: string
+  favorieteSpullen: string
+  favorieteArtiest: string
+  lievelingsMuziekNummer: string
+  geluksGetal: string
+  favorieteMuziekGenre: string
+  favorieteGame: string
+  favorieteFilm: string
+  favorieteSerie: string
+  hoogsteBedragOoitUitgegeven: string
+  waaraanHoogsteBedragOoitUitgegeven: string
+  lievelingsmerk: string
+  lievelingskleur: string
+  mooistePlekOoitGeweest: string
+  lelijkstePlekOoitGeweest: string
+  hoeveelUurBuiten: string
+  begroetingKennissen: string
+  bedtijd: string
+  opstaantijd: string
+  aantalGlazenWaterPerDag: string
+  aantalGlazenAlcoholPerWeek: string
+  lievelingsWeekdag: string
+  hoeveelMinutenPerDagOpSocialMedia: string
+  welkeSocialMediaGebruikt: string
+  stappenPerDagVolgensTelefoon: string
+  schermtijdPerDagVolgensTelefoon: string
+  theeOfKoffie: string
+  hoeveelGlazenTheeOfKoffiePerDag: string
+  etenAlsOntbijt: string
+  etenAlsLunch: string
+  etenAlsAvondeten: string
+  favorieteDrug: string
+  hoeVaakFavorieteDrugPerJaar: string
+  lievelingsgerecht: string
+  lievelingskeuken: string
+  afstandInMetersVanSupermarkt: string
+  tijdreisJaar: string
+  waaromTijdreizenNaarJaar: string
+  watMaaktVrolijk: string
+  meestInspirerendeMeegemaakt: string
+  gelovenInGoedEnKwaad: string
+  watMaaktOngemakkelijk: string
+  tevredenheidMetLeven: string
+  toekomstbeeld: string
+  hoeWasLevenVoorCorona: string
+  cijferRelatieTotTechnologie: string
+  cijferRelatieTotNatuur: string
+  perfectionistich: string
+  zonderGeluidOfZonderBeeldLeven: string
+  IntrovertOfExtrovert: string
+  zelfgekozenNaam: string
+  belangrijksteOnderwerpVanDezeTijd: string
+  isVerandertDoorWelkeOntmoetingOfGebeurtenis: string
+  verstePlekOoitgeweest: string
+  gesprokenTalen: string
+  gelovenInLevenBuitenPlaneet: string
+}
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
+const arrayMetArrays = data.map((data: DataType) => {
+  return Object.entries(data)
 })
+
+const arrayMetNummers = arrayMetArrays.map((arrayInArray) => {
+  const gefilterdeArray = arrayInArray.filter((data) => {
+    const num = parseInt(data[1])
+    if (!isNaN(num)) {
+      return data
+    }
+  })
+
+  const convertedArray = gefilterdeArray.map((data: [string, string]) => [
+    data[0],
+    parseInt(data[1])
+  ])
+
+  return convertedArray
+})
+
+console.log(arrayMetNummers)
